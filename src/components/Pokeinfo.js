73c5f9909dc558b584de0f9a75b208340063c2c5
Auguspo <augustpolet@gmail.com>
2023-03-19 20:13:12 -0300

    Guess view of the pokemon working

--- src/components/Pokeinfo.js
+++ src/components/Pokeinfo.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React from "react";
+import "../App.css";
 
-
-
-const Pokeinfo = ({info}) => {
-
-    return (<div >
-        {info.map((data) => {
-            return (
-              <div  key={data.id} className="container">
-                <img src={data.sprites["front_default"]}  alt={data.name}/>
-              </div>
-            );
-          })}
+const Pokeinfo = ({ info, active }) => {
+  return (
+    <div>
+      {info.map((data) => {
+        return (
+          <div>
+            {active && (
+              <img
+                className="img-light"
+                src={data.sprites["front_default"]}
+                alt={data.name}
+              />
+            )}
+            {active && <h2>{info[0].name.toUpperCase()}</h2>}
+            {!active && (
+              <img
+                className="img-dark"
+                src={data.sprites["front_default"]}
+                alt={data.name}
+              />
+            )}
           </div>
-    );
-}
+        );
+      })}
+    </div>
+  );
+};
 
 export default Pokeinfo;
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,11 +1,12 @@
-import React,{ useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Pokeinfo from "./Pokeinfo";
 import Streak from "./Streak";
-import "../App.css"
-import { useNavigate } from 'react-router-dom';
+import "../App.css";
+import { useNavigate } from "react-router-dom";
 
-const Pokemon=({ min, max }) => {
+function Pokemon({ min, max }) {
+  
   useEffect(() => {
     getPokemon();
   }, []);
@@ -14,8 +15,13 @@ const Pokemon=({ min, max }) => {
   const [pokemonData, setPokemonData] = useState([]);
   const [guess, setGuess] = useState("");
   const [count, setCount] = useState(0);
+  const [isActive, setIsActive] = useState(false);
   const navigate = useNavigate();
   let num = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  function timeout(delay) {
+    return new Promise((res) => setTimeout(res, delay));
+  }
 
   const handleGuess = (e) => {
     setGuess(e.target.value);
@@ -23,20 +29,24 @@ const Pokemon=({ min, max }) => {
 
   const submitGuess = (e) => {
     e.preventDefault();
+    setIsActive(true);
     if (guess.toLowerCase() === pokemon.replace("-", " ")) {
       alert("Correcto");
+
       setCount(count + 1);
     } else {
       alert("Prueba de nuevo");
       setCount(0);
     }
-    
+
+    getPokemon();
 
     setGuess("");
   };
 
   const getPokemon = async () => {
     const toArray = [];
+    await timeout(1000);
     try {
       const url = `https://pokeapi.co/api/v2/pokemon/${num}`;
       const res = await axios.get(url);
@@ -47,21 +57,26 @@ const Pokemon=({ min, max }) => {
     } catch (e) {
       console.log(e);
     }
+    setIsActive(false);
   };
 
   return (
-    <div className='App'>
-     
-      <Pokeinfo info={pokemonData} />
+    <div className="App">
+      <Pokeinfo info={pokemonData} active={isActive} />
+
       <Streak value={count} />
 
       <form onSubmit={submitGuess}>
         <input value={guess} onChange={handleGuess}></input>
         <button type="submit">Guess</button>
       </form>
-      <button onClick={() => {
-		navigate('/', {replace: true});
-	}}>Cambiar Generación</button>	
+      <button
+        onClick={() => {
+          navigate("/", { replace: true });
+        }}
+      >
+        Cambiar Generación
+      </button>
     </div>
   );
 }
