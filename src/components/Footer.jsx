15cf28482197537c9ff690bb23a241bd70837650
Auguspo <augustpolet@gmail.com>
2023-04-03 18:40:50 -0300

    new Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,16 @@ import React from "react";
 function Footer() {
   return (
    
-<footer class=" left-0 min-w-full fixed bottom-0 w-full sm:w-auto bottom-0 overflow-visible   z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-     <a href="https://flowbite.com/" class="hover:underline">© 2023 Augusto Poletti</a>
+<footer className=" left-0 min-w-full fixed bottom-0 w-full sm:w-auto bottom-0 overflow-visible   z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+     <a href="https://flowbite.com/" className="hover:underline">© 2023 Augusto Poletti</a>
    
-    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Linkedin</a>
+            <a href="#" className="mr-4 hover:underline md:mr-6">Linkedin</a>
         </li>
 
         <li>
-            <a href="#" class="hover:underline">Github</a>
+            <a href="#" className="hover:underline">Github</a>
         </li>
     </ul>
 </footer>
